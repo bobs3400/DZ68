b0e69d71821e2022ac2e2d13a4021a7898ac506e
bobs3400 <124318533+bobs3400@users.noreply.github.com>
2023-03-11 17:25:29 +1000

    68

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+Clear();
+Write("Введите число M: ");
+int m = int.Parse(ReadLine()!);
+Write("Введите число N: ");
+int n = int.Parse(ReadLine()!);
+WriteLine(Akerman(m, n));
+
+int Akerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if ((m > 0) && (n == 0))
+    {
+        return Akerman(m - 1, 1);
+    }
+    else {
+        return Akerman(m - 1, Akerman(m, n - 1));
+    }
+}
